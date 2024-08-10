6c74bdd4cb0cb9e4b9edea31458c5c4b8c22923b
Neo Wang <wiirhan@163.com>
2024-08-10 12:15:27 +0800

    chore: Add jsdom npm dependency for testing environment

--- shims.d.ts
+++ shims.d.ts
@@ -1,17 +1,18 @@
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
 
-  const component: DefineComponent<object, object, any>
-  export default component
+  const component: DefineComponent<object, object, any>;
+  export default component;
 }
 
+/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VUE_APP_BASE_API: string
+  readonly VUE_APP_BASE_API: string;
   // 更多环境变量...
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
